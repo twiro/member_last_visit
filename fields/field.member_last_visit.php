e641ecba70729926b4c96fd36eedcf2739d3f1e8
Roman Klein <web@romanklein.com>
2017-12-04 09:41:43 +0100

    Release 2.0.0
    - Code clean up
    - Styling improvements: uses the 'field-value-readonly'-class now
    (Symphony 2.6.5 +)
    - Compatibility with Symphony 2.7.x
    ([#4](https://github.com/twiro/member_last_visit/issues/4)
    - PHP 7 support

--- fields/field.member_last_visit.php
+++ fields/field.member_last_visit.php
@@ -1,102 +1,206 @@
 <?php
 
+if (!defined('__IN_SYMPHONY__')) die('<h2>Symphony Error</h2><p>You cannot directly access this file</p>');
 
-	Class fieldMember_Last_Visit extends Field {
+Class fieldMember_Last_Visit extends Field
+{
+    /*-------------------------------------------------------------------------
+        DEFINITION
+    -------------------------------------------------------------------------*/
 
-	/*-------------------------------------------------------------------------
-		Definition:
-	-------------------------------------------------------------------------*/
+    /**
+     *
+     * constructor for the Field object
+     * @param mixed $parent
+     *
+     * @since 1.0.0
+     */
 
-		public function __construct(){
-			parent::__construct();
-			$this->_name = __('Member: Last Visit');
-			$this->_showassociation = false;
-		}
+    public function __construct()
+    {
+        // call the parent constructor
+        parent::__construct();
+        // set the name of the field
+        $this->_name = __('Member: Last Visit');
+        // ???
+        $this->_showassociation = false;
+    }
 
-		public function isSortable(){
-			return true;
-		}
+    public function isSortable()
+    {
+        return true;
+    }
 
-		public function mustBeUnique(){
-			return true;
-		}
+    public function canFilter()
+    {
+        return true;
+    }
 
-	/*-------------------------------------------------------------------------
-		Setup:
-	-------------------------------------------------------------------------*/
+    public function mustBeUnique()
+    {
+        return true;
+    }
 
-		public function createTable(){
-			return Symphony::Database()->query("
-				CREATE TABLE IF NOT EXISTS `tbl_entries_data_" . $this->get('id') . "` (
-				  `id` int(11) unsigned NOT NULL auto_increment,
-				  `entry_id` int(11) unsigned NOT NULL,
-				  `value` varchar(255) default NULL,
-				  PRIMARY KEY  (`id`),
-				  KEY `value` (`value`),
-				  UNIQUE KEY `entry_id` (`entry_id`)
-				) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-			");
-		}
+    public function canImport()
+    {
+        return false;
+    }
+    public function canPrePopulate()
+    {
+        return false;
+    }
 
-	/*-------------------------------------------------------------------------
-		Settings:
-	-------------------------------------------------------------------------*/
 
-		public function displaySettingsPanel(&$wrapper, $errors=NULL){
-			parent::displaySettingsPanel($wrapper, $errors);
+    /*-------------------------------------------------------------------------
+        SETUP
+    -------------------------------------------------------------------------*/
 
-			$div = new XMLElement('div', null, array('class' => 'compact'));
-			$this->appendRequiredCheckbox($div);
-			$this->appendShowColumnCheckbox($div);
-			$wrapper->appendChild($div);
-		}
+    /**
+     * create table
+     *
+     * Creates the table needed for the extensions entries
+     */
 
-	/*-------------------------------------------------------------------------
-		Output:
-	-------------------------------------------------------------------------*/
+    public function createTable()
+    {
+        $tbl = "tbl_entries_data_" . $this->get('id');
 
-		public function appendFormattedElement($wrapper, $data, $encode = false) {
-			if(isset($data['value']) && !is_null($data['value'])) {
+        return Symphony::Database()->query("
+            CREATE TABLE IF NOT EXISTS `$tbl` (
+                `id` int(11) unsigned NOT NULL auto_increment,
+                `entry_id` int(11) unsigned NOT NULL,
+                `value` varchar(255) default NULL,
+                PRIMARY KEY  (`id`),
+                KEY `value` (`value`),
+                UNIQUE KEY `entry_id` (`entry_id`)
+            ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+        ");
+    }
 
-				// Get date
-				if(is_array($data['value'])) {
-					$date = current($data['value']);
-				}
-				else {
-					$date = $data['value'];
-				}
 
-				// Append date
-				$wrapper->appendChild(General::createXMLDateObject($date, $this->get('element_name')));
-			}
-		}
+    /*-------------------------------------------------------------------------
+        SETTINGS
+    -------------------------------------------------------------------------*/
 
-		public function prepareTableValue($data, XMLElement $link=NULL, $entry_id = null) {
-			$value = null;
+    /**
+     *
+     * display settings panel
+     *
+     * @since version 1.0.0
+     */
 
-			if(isset($data['value'])) {
-				$value = DateTimeObj::get(__SYM_DATETIME_FORMAT__, strtotime($data['value']));
-			}
+    public function displaySettingsPanel(XMLElement &$wrapper, $errors = null)
+    {
+        parent::displaySettingsPanel($wrapper, $errors);
 
-			return parent::prepareTableValue(array('value' => $value), $link, $entry_id = null);
-		}
+        $div = new XMLElement('div', null, array('class' => 'compact'));
+        $this->appendRequiredCheckbox($div);
+        $this->appendShowColumnCheckbox($div);
+        $wrapper->appendChild($div);
+    }
 
-		public function displayPublishPanel(XMLElement &$wrapper, $data = null, $error = null, $prefix = null, $postfix = null, $entry_id = null) {
-			$label = Widget::Label($this->get('label'));
-			$name = $this->get('element_name');
+    /**
+     *
+     * commit field settings
+     *
+     * @since version 2.0.0
+     */
 
-			if (isset($data['value'])) {
-				$label->appendChild(
-					new XMLElement('i', DateTimeObj::get(__SYM_DATETIME_FORMAT__, strtotime($data['value'])))
-				);
-				$label->appendChild(
-					Widget::Input("fields{$prefix}[{$name}]", $data['value'], 'hidden')
-				);
-			} else {
-				$label->appendChild(
-					new XMLElement('i', __('No visit yet.'))
-				);
-			}
-			$wrapper->appendChild($label);
-		}
-	}
+    public function commit()
+    {
+        if (!parent::commit()) return false;
+        $id = $this->get('id');
+        if ($id === false) return false;
+        return FieldManager::saveSettings($id, $settings=null);
+    }
+
+
+    /*-------------------------------------------------------------------------
+        DATA SOURCE OUTPUT
+    -------------------------------------------------------------------------*/
+
+    /**
+     *
+     * append formmatted element
+     *
+     * @since version 1.0.0
+     */
+
+    public function appendFormattedElement(XMLElement &$wrapper, $data, $encode = false, $mode = null, $entry_id = null)
+    {
+        if(isset($data['value']) && !is_null($data['value'])) {
+
+            // Get date
+            if(is_array($data['value'])) {
+                $date = current($data['value']);
+            } else {
+                $date = $data['value'];
+            }
+
+            // Append date
+            $wrapper->appendChild(General::createXMLDateObject($date, $this->get('element_name')));
+        }
+    }
+
+
+    /*-------------------------------------------------------------------------
+        UI
+    -------------------------------------------------------------------------*/
+
+    /**
+     *
+     * prepare table value
+     *
+     * @since version 1.0.0
+     */
+
+    public function prepareTableValue($data, XMLElement $link = null, $entry_id = null)
+    {
+        $value = null;
+
+        if (isset($data['value'])) {
+            $value = DateTimeObj::get(__SYM_DATETIME_FORMAT__, strtotime($data['value']));
+        }
+
+        return parent::prepareTableValue(array('value' => $value), $link, $entry_id = null);
+    }
+
+    /**
+     *
+     * display publish panel
+     *
+     * @since version 1.0.0
+     */
+
+    public function displayPublishPanel(XMLElement &$wrapper, $data = null, $flagWithError = null, $fieldnamePrefix = null, $fieldnamePostfix = null, $entry_id = null)
+    {
+        $label = Widget::Label($this->get('label'));
+        $name = $this->get('element_name');
+
+        $datetime_formatted = DateTimeObj::get(__SYM_DATETIME_FORMAT__, strtotime($data['value']));
+
+        // due to styling changes in symphony 2.6.5 we need to switch from <i> to <time>
+
+        $symphony_version = Symphony::Configuration()->get('version', 'symphony');
+        if (version_compare($symphony_version, '2.6.5', '<')) {
+            $html_element = 'i';
+        } else {
+            $html_element = 'time';
+        }
+
+        if (isset($data['value'])) {
+            $label->appendChild(
+                new XMLElement($html_element, $datetime_formatted, array('class' => 'field-value-readonly'))
+            );
+            $label->appendChild(
+                Widget::Input("fields{$prefix}[{$name}]", $data['value'], 'hidden')
+            );
+        } else {
+            $label->appendChild(
+                new XMLElement($html_element, __('No visit yet.'), array('class' => 'field-value-readonly'))
+            );
+        }
+
+        $wrapper->appendChild($label);
+    }
+}
